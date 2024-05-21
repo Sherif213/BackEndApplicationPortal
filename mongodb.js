1cbcf508782c0f6d20d3898cc64231e7a916dbcc
Şerif Elashry <Shouldtheone@gmail.com>
2024-05-22 02:22:12 +0300

    Database

--- mongodb.js
+++ mongodb.js
@@ -1,14 +1,17 @@
-// mongodb.js
-
 const mongoose = require('mongoose');
 
 const username = 'shouldtheone';
 const password = 'uEp5Xb4UobcnxsYD';
 const clusterUrl = 'cluster0.l69ivke.mongodb.net';
 const dbName = 'unesco_application';
-const uri = `mongodb+srv://${username}:${password}@${clusterUrl}/${dbName}?retryWrites=true&w=majority&appName=Cluster0`;
 
-mongoose.connect(uri).then(() => {
+// Updated URI with ssl=true parameter
+const uri = `mongodb+srv://${username}:${password}@${clusterUrl}/${dbName}?retryWrites=true&w=majority&appName=Cluster0&ssl=true`;
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
     console.log("Connected to MongoDB Atlas");
 }).catch(err => {
     console.error("Error connecting to MongoDB Atlas", err);
