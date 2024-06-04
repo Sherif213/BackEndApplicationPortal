7e976c86f84adca782b548e2e54a6767f77e83d9
Şerif Elashry <Shouldtheone@gmail.com>
2024-06-04 12:16:12 +0300

    Changes

--- JuniorPeacePolicy.php
+++ JuniorPeacePolicy.php
@@ -1,12 +1,13 @@
-<?php
-session_start();
-?>
-<?php include("include/Policy.php");?>
+
+<?php include "include/PolicyHeader.php" ?>
+            <?php include "include/JuniorPeace.php" ?>
+
+            <?php include "include/PolicyFooter.php" ?>
                 <a href="JuniorPeace.php"><button type="submit" class="btn btn-success">Okay, I agree</button></a>
                 &nbsp; <a href="index.php"><button type="button" class="btn btn-danger">Cancel</button></a>
             </div>
 
-            <?php include "include/footer.php"?>
+            <?php include "include/footer.php" ?>
         </div>
     </div>
 
--- SummerDiplomacyPolicy.php
+++ SummerDiplomacyPolicy.php
@@ -1,12 +1,13 @@
-<?php
-session_start();
-?>
-<?php include("include/Policy.php");?>
+
+<?php include "include/PolicyHeader.php" ?>
+            <?php include "include/SummerDiplomacy.php" ?>
+
+            <?php include "include/PolicyFooter.php" ?>
                 <a href="SummerDiplomacy.php"><button type="submit" class="btn btn-success">Okay, I agree</button></a>
                 &nbsp; <a href="index.php"><button type="button" class="btn btn-danger">Cancel</button></a>
             </div>
 
-            <?php include "include/footer.php"?>
+            <?php include "include/footer.php" ?>
         </div>
     </div>
 
