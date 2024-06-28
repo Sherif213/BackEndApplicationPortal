c4962376905c0f969eb62fece41be87b4ae0ef65
Serif Elashry <shouldtheone@gmail.com>
2024-06-28 14:25:18 +0300

    changes

---
+++ include/summerPolicyFooter.php
@@ -0,0 +1,14 @@
+<!-- Text Section -->
+<div class="text-section">
+
+</div>
+
+<!-- Information Container -->
+<div class="information-container" style="text-align:justify; font-size:14px !important;">
+    <span class="text">‘The UNESCO Summer Diplomacy Program provides university students an unparalleled opportunity to immerse themselves in a rich blend of educational and cultural activities.
+    <br><br> Over the two-week period, participants will delve into peace education, intercultural integration, and global cooperation. The program includes interactive sessions, workshops, and tours of Istanbul's significant cultural landmarks, designed to broaden perspectives and equip students with the tools necessary to advocate for a more peaceful and sustainable world.
+    <br><br>Students will benefit from a comprehensive array of educational activities, including specialized courses and workshops on diplomacy, international relations, and human rights.
+    <br><br>Topics such as the history and traditions of diplomacy, power dynamics, and public diplomacy strategies will be explored in depth. Interactive sessions will further engage participants in subjects like conflict resolution, storytelling, and human rights advocacy, encouraging critical thinking and empathy.
+    <br><br> <strong>Join us at the UNESCO Summer Diplomacy Program in Istanbul and become part of a global community committed to building a more peaceful and interconnected world.</strong>’
+    <div class="clear"><br><br></span></div>
+    <input type="hidden" name="_IsAgree" value="1">
