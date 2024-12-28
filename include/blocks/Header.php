f4d2e725cefcf104c208f5c7a9b681e7c7870cb4
Sherif213 <shouldtheone@gmail.com>
2024-12-28 18:14:06 +0300

    Fix: Meta tag

--- include/blocks/Header.php
+++ include/blocks/Header.php
@@ -1,6 +1,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Serif Elashry">
+    <meta name="description" content="UNESCO Application Form for Aydin University">
+    <meta name="keywords" content="UNESCO, Application, Aydin University, Form">
+    <meta name="robots" content="index, follow">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="shortcut icon" type="aydin University" href="images/IAU.png">
     <title>UNESCO APPLICATION FORM</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
